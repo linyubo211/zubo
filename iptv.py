7b324545771d280d26a30ca128667eb4b0665fa6
linyubo211 <linyubo211@outlook.com>
2026-04-03 11:06:19 +0800

    Update iptv.py

--- iptv.py
+++ iptv.py
@@ -23,8 +23,7 @@ if not CONFIG_FILE:
     CONFIG_FILE = os.path.join(BASE_DIR, "iptv_config.json")
 OUTPUT_FILE = os.path.join(BASE_DIR, "IPTV.txt")
 DEFAULT_THIRD_PARTY_URLS = OrderedDict([
-    ("https://raw.githubusercontent.com/kakaxi-1/IPTV/main/iptv.txt", "source1.txt"),
-    ("https://raw.githubusercontent.com/kakaxi-1/zubo/main/IPTV.txt", "source2.txt"),
+    ("https://raw.githubusercontent.com/yubaomo02/ol/refs/heads/main/zubo_live.txt", "source1.txt"),
 ])
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
